bee494fa303e54c7d74ee1a5083ff7fdf2526de3
ecstasyego <70621679+ecstasyego@users.noreply.github.com>
2024-08-15 20:45:02 +0900

    Create qpushbutton.py

---
+++ widget/input/qpushbutton.py
@@ -0,0 +1,28 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Window(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+        layout = QtWidgets.QVBoxLayout()
+
+        widget1 = QtWidgets.QPushButton('&Button1', self)
+        widget1.setCheckable(True)
+        widget1.toggle()
+        widget2 = QtWidgets.QPushButton(self)
+        widget2.setText('Button&2')
+        widget3 = QtWidgets.QPushButton('Button3', self)
+        widget3.setEnabled(False)
+
+        layout.addWidget(widget1)
+        layout.addWidget(widget2)
+        layout.addWidget(widget3)
+        self.setLayout(layout)
+        self.setGeometry(300, 300, 300, 200)
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+
+    window = Window()
+    window.show()
+    sys.exit(app.exec_())
