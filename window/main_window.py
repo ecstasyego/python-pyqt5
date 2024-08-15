102f6aa2bc108d8312313b9c134e00e7ef89e2ff
ecstasyego <70621679+ecstasyego@users.noreply.github.com>
2024-08-15 21:04:33 +0900

    Create main_window.py

---
+++ window/main_window.py
@@ -0,0 +1,25 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Window(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle('Window Title')
+        self.setGeometry(300, 300, 300, 200)
+
+        statusbar = self.statusBar()
+        statusbar.showMessage('Ready')
+
+        menubar = self.menuBar()
+        menubar.setNativeMenuBar(False)
+        menubar.addMenu('&File')
+
+        toolbar = self.addToolBar('Exit')
+        toolbar.addAction(QtWidgets.QAction(QtGui.QIcon('exit.png'), 'Exit', self))
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+
+    window = Window()
+    window.show()
+    sys.exit(app.exec_())
