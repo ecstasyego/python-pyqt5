ea70d3a680fa707c85f56f21b138c68a792b88a5
ecstasyego <70621679+ecstasyego@users.noreply.github.com>
2024-08-16 01:12:26 +0900

    Update qstatusbar.py

--- widget/display/qstatusbar.py
+++ widget/display/qstatusbar.py
@@ -3,20 +3,22 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Window(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
-        # Widgets
-        widget1 = QtWidgets.QStatusBar()
-        widget2 = QtWidgets.QStatusBar()
-        widget3 = QtWidgets.QStatusBar()
-        widget1.showMessage("status1", 1000) # milliseconds
-        widget2.showMessage("status2", 2000) # milliseconds
-        widget3.showMessage("status3", 3000) # milliseconds
+        # WIDGETS: Declaration
+        self.widgets = dict()
+        self.widgets['widget1'] = QtWidgets.QStatusBar(self)
+        self.widgets['widget2'] = QtWidgets.QStatusBar(self)
+        self.widgets['widget3'] = QtWidgets.QStatusBar(self)
+
+        # WIDGETS: Properties
+        self.widgets['widget1'].showMessage("status1", 1000) # milliseconds
+        self.widgets['widget2'].showMessage("status2", 2000) # milliseconds
+        self.widgets['widget3'].showMessage("status3", 3000) # milliseconds
 
         # Layout
         layout = QtWidgets.QVBoxLayout()
-        layout.addStretch(1)
-        layout.addWidget(widget1)
-        layout.addWidget(widget2)
-        layout.addWidget(widget3)
+        layout.addWidget(self.widgets['widget1'])
+        layout.addWidget(self.widgets['widget2'])
+        layout.addWidget(self.widgets['widget3'])
         layout.addStretch(1)
         self.setLayout(layout)
         self.setGeometry(300, 300, 300, 200)
