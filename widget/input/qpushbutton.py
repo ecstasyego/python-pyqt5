4392d1f4eeebddf249aadee06757d8b7bda00865
ecstasyego <70621679+ecstasyego@users.noreply.github.com>
2024-08-16 00:49:20 +0900

    Update qpushbutton.py

--- widget/input/qpushbutton.py
+++ widget/input/qpushbutton.py
@@ -4,46 +4,31 @@ class Window(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
         # WIDGETS
-        widget1 = QtWidgets.QPushButton('&Button1', self); widget1.setCheckable(True); widget1.toggle()
-        widget2 = QtWidgets.QPushButton(self); widget2.setText('Button&2')
-        widget3 = QtWidgets.QPushButton('Button3', self); widget3.setEnabled(False)
+        self.widgets = dict()
+        self.widgets['widget1'] = QtWidgets.QPushButton('&Button1', self)
+        self.widgets['widget2'] = QtWidgets.QPushButton(self)
+        self.widgets['widget3'] = QtWidgets.QPushButton('Button3', self)
+        self.widgets['widget4'] = QtWidgets.QStatusBar(self)
+
+        self.widgets['widget1'].setCheckable(True)
+        self.widgets['widget1'].toggle()
+        self.widgets['widget2'].setText('Button&2')
+        self.widgets['widget3'].setEnabled(False)
 
         # LAYOUT
         layout = QtWidgets.QVBoxLayout()
-        layout.addWidget(widget1)
-        layout.addWidget(widget2)
-        layout.addWidget(widget3)
+        layout.addWidget(self.widgets['widget1'])
+        layout.addWidget(self.widgets['widget2'])
+        layout.addWidget(self.widgets['widget3'])
         layout.addStretch(1)
-        layout.addWidget(QtWidgets.QStatusBar())
+        layout.addWidget(self.widgets['widget4'])
         self.setLayout(layout)
         self.setGeometry(300, 300, 300, 200)
 
     def callback(self):
-        status_widget = self.get_widgets()[-1]
-        status_widget.showMessage("CallBack", 1000)
+        self.widgets['widget4'].showMessage("CallBack", 1000)
 
-    def get_widgets(self, layout=None):
-        layout = self.layout() if layout is None else layout
-        widgets = list()
-        for i in range(layout.count()):
-            item = layout.itemAt(i)
-            if item is not None:
-                widget = item.widget()
-                if widget is not None:
-                    widgets.append(widget)
-        return widgets
 
-    def get_sublayouts(self, layout=None):
-        layout = self.layout() if layout is None else layout
-        sublayouts = list()
-        for i in range(layout.count()):
-            item = layout.itemAt(i)
-            if item is not None:
-                sublayout = item.layout()
-                if sublayout is not None:
-                    sublayouts.append(sublayout)
-        return sublayouts
-        
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
