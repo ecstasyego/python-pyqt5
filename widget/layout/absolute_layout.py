212a4dcff2764b59d357c067affa460eedff2723
ecstasyego <70621679+ecstasyego@users.noreply.github.com>
2024-08-15 21:59:26 +0900

    Create absolute_layout.py

---
+++ widget/layout/absolute_layout.py
@@ -0,0 +1,16 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Window(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+        widget = QtWidgets.QWidget(self)
+        widget.move(20, 20)
+        self.setGeometry(300, 300, 300, 200)
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+
+    window = Window()
+    window.show()
+    sys.exit(app.exec_())
