796e9a68f65b16bcc3c30f3dfacf4520e48b4c81
ecstasyego <70621679+ecstasyego@users.noreply.github.com>
2024-08-15 19:31:32 +0900

    Update qwidget.py

--- widget/container/qwidget.py
+++ widget/container/qwidget.py
@@ -1,1 +1,15 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
 
+class Window(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setLayout(QtWidgets.QVBoxLayout())
+        self.setGeometry(300, 300, 300, 200)
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+
+    window = Window()
+    window.show()
+    sys.exit(app.exec_())
