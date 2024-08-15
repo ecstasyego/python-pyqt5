c77404d32fc6b71f3ac237b952a107ab4ecbbf29
ecstasyego <70621679+ecstasyego@users.noreply.github.com>
2024-08-16 03:18:34 +0900

    Create qdial.py

---
+++ widget/input/qdial.py
@@ -0,0 +1,34 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Window(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+        # WIDGETS: Declaration
+        self.widgets = dict()
+        self.widgets['widget1'] = QtWidgets.QDial(self)
+        self.widgets['widget2'] = QtWidgets.QStatusBar(self)
+        # WIDGETS: Properties
+        self.widgets['widget1'].setRange(0, 50)
+        self.widgets['widget1'].setSingleStep(2)
+        self.widgets['widget1'].valueChanged.connect(self.callback)
+
+        # LAYOUT
+        layout = QtWidgets.QVBoxLayout()
+        layout.addWidget(self.widgets['widget1'])
+        layout.addStretch(1)
+        layout.addWidget(self.widgets['widget2'])
+        self.setLayout(layout)
+        self.setGeometry(300, 300, 300, 200)
+
+    def callback(self):
+        value = self.widgets['widget1'].value()
+        self.widgets['widget2'].showMessage(f"Current value: {value}", 1000)
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+
+    window = Window()
+    window.show()
+    sys.exit(app.exec_())
