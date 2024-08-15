db63a429f144bb74d4e751cbd1b9b05d8929592e
ecstasyego <70621679+ecstasyego@users.noreply.github.com>
2024-08-15 19:35:23 +0900

    Create qstackedwidget.py

---
+++ widget/container/qstackedwidget.py
@@ -0,0 +1,21 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Window(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+        layout = QtWidgets.QVBoxLayout()
+        stackedWidget = QtWidgets.QStackedWidget()
+        stackedWidget.addWidget(QtWidgets.QWidget())
+        stackedWidget.addWidget(QtWidgets.QWidget())
+        layout.addWidget(stackedWidget)
+
+        self.setLayout(layout)
+        self.setGeometry(300, 300, 300, 200)
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+
+    window = Window()
+    window.show()
+    sys.exit(app.exec_())
