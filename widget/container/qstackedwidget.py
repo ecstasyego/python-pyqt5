6b8c5b49cafb26c6a575f89b4f7d8a952afe4419
ecstasyego <70621679+ecstasyego@users.noreply.github.com>
2024-08-15 20:12:47 +0900

    Update qstackedwidget.py

--- widget/container/qstackedwidget.py
+++ widget/container/qstackedwidget.py
@@ -4,11 +4,11 @@ class Window(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
         layout = QtWidgets.QVBoxLayout()
-        stackedWidget = QtWidgets.QStackedWidget()
-        stackedWidget.addWidget(QtWidgets.QWidget())
-        stackedWidget.addWidget(QtWidgets.QWidget())
-        layout.addWidget(stackedWidget)
-
+        widget = QtWidgets.QStackedWidget()
+        widget.addWidget(QtWidgets.QWidget())
+        widget.addWidget(QtWidgets.QWidget())
+        
+        layout.addWidget(widget)
         self.setLayout(layout)
         self.setGeometry(300, 300, 300, 200)
 
