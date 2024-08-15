654979def3ee25f7311400a3a147629081e4dce0
ecstasyego <70621679+ecstasyego@users.noreply.github.com>
2024-08-15 22:09:21 +0900

    Create get_widget.py

---
+++ layout/get_widget.py
@@ -0,0 +1,26 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Window(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+        layout = QtWidgets.QVBoxLayout()
+        layout.addWidget(QtWidgets.QWidget())
+        layout.addWidget(QtWidgets.QWidget())
+        layout.addWidget(QtWidgets.QWidget())
+      
+        self.setLayout(layout)
+        self.setGeometry(300, 300, 300, 200)
+
+        # widget = layout.itemAt(index).widget()
+        widget1 = layout.itemAt(0).widget()
+        widget2 = layout.itemAt(1).widget()
+        widget3 = layout.itemAt(2).widget()
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+
+    window = Window()
+    window.show()
+    sys.exit(app.exec_())
