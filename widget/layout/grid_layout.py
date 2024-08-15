c79817102f3df661757ce96c67cf4e58f30ca4ff
ecstasyego <70621679+ecstasyego@users.noreply.github.com>
2024-08-15 20:30:56 +0900

    Update grid_layout.py

--- widget/layout/grid_layout.py
+++ widget/layout/grid_layout.py
@@ -7,9 +7,9 @@ class Window(QtWidgets.QWidget):
         layout.addWidget(QtWidgets.QWidget(), 0, 0)
         layout.addWidget(QtWidgets.QWidget(), 1, 0)
         layout.addWidget(QtWidgets.QWidget(), 2, 0)
-        layout.addWidget(QtWidgets.QWidget(), 0, 1)
-        layout.addWidget(QtWidgets.QWidget(), 1, 1)
-        layout.addWidget(QtWidgets.QWidget(), 2, 1)
+        layout.addLayout(QtWidgets.QVBoxLayout(), 0, 1)
+        layout.addLayout(QtWidgets.QVBoxLayout(), 1, 1)
+        layout.addLayout(QtWidgets.QVBoxLayout(), 2, 1)
 
         self.setLayout(layout)
         self.setGeometry(300, 300, 300, 200)
