c4701a12c50e83208e3f08e94daf81ecb24fca78
ecstasyego <70621679+ecstasyego@users.noreply.github.com>
2024-08-15 20:11:26 +0900

    Create qsplitter.py

---
+++ widget/container/qsplitter.py
@@ -0,0 +1,22 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Window(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+        layout = QtWidgets.QVBoxLayout()
+        widget = QtWidgets.QSplitter(QtCore.Qt.Vertical)
+        widget.addWidget(QtWidgets.QWidget())
+        widget.addWidget(QtWidgets.QSplitter(QtCore.Qt.Horizontal))
+        widget.addWidget(QtWidgets.QWidget())
+
+        layout.addWidget(widget)
+        self.setLayout(layout)
+        self.setGeometry(300, 300, 300, 200)
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+
+    window = Window()
+    window.show()
+    sys.exit(app.exec_())
