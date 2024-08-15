5c0d269a879ac49c35a152321ec43e006daf66d6
ecstasyego <70621679+ecstasyego@users.noreply.github.com>
2024-08-15 19:28:11 +0900

    Update and rename qtabwidget_.py to qwidget.py

--- widget/qwidget.py
+++ widget/qwidget.py
@@ -3,13 +3,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Window(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
-        layout = QtWidgets.QVBoxLayout()
-        tabs = QtWidgets.QTabWidget()
-        tabs.addTab(QtWidgets.QWidget(), 'TAB1')
-        tabs.addTab(QtWidgets.QWidget(), 'TAB2')
-        layout.addWidget(tabs)
-
-        self.setLayout(layout)
+        self.setLayout(QtWidgets.QVBoxLayout())
         self.setGeometry(300, 300, 300, 200)
 
 if __name__ == "__main__":
