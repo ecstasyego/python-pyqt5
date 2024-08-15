c13d17d35a6061a5613f2772346eff3b40faaa54
ecstasyego <70621679+ecstasyego@users.noreply.github.com>
2024-08-16 00:23:33 +0900

    Create qtime.py

---
+++ widget/timer/qtime.py
@@ -0,0 +1,25 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Window(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+        # TIME
+        time = QtCore.QTime.currentTime().toString('HH:mm:ss')
+
+        # WIDGETS
+        widget = QtWidgets.QLabel(self)
+        widget.setText(f'Current Time: {time}')
+
+        # LAYOUT
+        layout = QtWidgets.QVBoxLayout()
+        layout.addWidget(widget)
+        self.setLayout(layout)
+        self.setGeometry(300, 300, 300, 200)
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+
+    window = Window()
+    window.show()
+    sys.exit(app.exec_())
