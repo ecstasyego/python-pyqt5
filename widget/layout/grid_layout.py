5537fbd3ea3ba3f54b8bb44fc13231ebff627311
ecstasyego <70621679+ecstasyego@users.noreply.github.com>
2024-08-15 20:27:34 +0900

    Create grid_layout.py

---
+++ widget/layout/grid_layout.py
@@ -0,0 +1,23 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Window(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+        layout = QtWidgets.QGridLayout()
+        layout.addWidget(QtWidgets.QWidget(), 0, 0)
+        layout.addWidget(QtWidgets.QWidget(), 1, 0)
+        layout.addWidget(QtWidgets.QWidget(), 2, 0)
+        layout.addWidget(QtWidgets.QWidget(), 0, 1)
+        layout.addWidget(QtWidgets.QWidget(), 1, 1)
+        layout.addWidget(QtWidgets.QWidget(), 2, 1)
+
+        self.setLayout(layout)
+        self.setGeometry(300, 300, 300, 200)
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+
+    window = Window()
+    window.show()
+    sys.exit(app.exec_())
