b05704c41dce1172c50e162ccc575c0b0906042b
ecstasyego <70621679+ecstasyego@users.noreply.github.com>
2024-08-15 22:54:29 +0900

    Create qwidget_layout.py

---
+++ widget/layout/qwidget_layout.py
@@ -0,0 +1,30 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Window(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setLayout(QtWidgets.QVBoxLayout())
+        self.setGeometry(300, 300, 300, 200)
+
+        self.widgets = list()
+        self.sublayouts = list()
+        layout = self.layout()
+        for i in range(layout.count()):
+            item = layout.itemAt(i)
+            if item is not None:
+                widget = item.widget()
+                if widget is not None:
+                    self.widgets.append(widget)
+
+                sublayout = item.layout()
+                if sublayout is not None:
+                    self.sublayouts.append(sublayout)
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+
+    window = Window()
+    window.show()
+    sys.exit(app.exec_())
