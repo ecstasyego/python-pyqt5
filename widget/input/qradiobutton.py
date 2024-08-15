793927de31f8c8691f2ba37531c4609fc23134a1
ecstasyego <70621679+ecstasyego@users.noreply.github.com>
2024-08-16 00:45:24 +0900

    Update qradiobutton.py

--- widget/input/qradiobutton.py
+++ widget/input/qradiobutton.py
@@ -4,48 +4,30 @@ class Window(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
         # WIDGETS
-        widget1 = QtWidgets.QRadioButton("Option1", self); widget1.setChecked(True)
-        widget2 = QtWidgets.QRadioButton(self); widget2.setText("Option2")
+        self.widgets = dict()
+        self.widgets['widget1'] = QtWidgets.QRadioButton("Option1", self);
+        self.widgets['widget2'] = QtWidgets.QRadioButton(self);
+        self.widgets['widget3'] = QtWidgets.QStatusBar()
+
+        self.widgets['widget1'].setChecked(True)
+        self.widgets['widget2'].setText("Option2")
 
         # LAYOUT
         layout = QtWidgets.QVBoxLayout()
-        layout.addWidget(widget1)
-        layout.addWidget(widget2)
+        layout.addWidget(self.widgets['widget1'])
+        layout.addWidget(self.widgets['widget2'])
         layout.addStretch(1)
-        layout.addWidget(QtWidgets.QStatusBar())
+        layout.addWidget(self.widgets['widget3'])
         self.setLayout(layout)
         self.setGeometry(300, 300, 300, 200)
 
     def callback(self):
-        status_widget = self.get_widgets()[-1]
-        status_widget.showMessage("CallBack", 1000)
+        self.widgets['widget3'].showMessage("CallBack", 1000)
 
-    def get_widgets(self, layout=None):
-        layout = self.layout() if layout is None else layout
-        widgets = list()
-        for i in range(layout.count()):
-            item = layout.itemAt(i)
-            if item is not None:
-                widget = item.widget()
-                if widget is not None:
-                    widgets.append(widget)
-        return widgets
-
-    def get_sublayouts(self, layout=None):
-        layout = self.layout() if layout is None else layout
-        sublayouts = list()
-        for i in range(layout.count()):
-            item = layout.itemAt(i)
-            if item is not None:
-                sublayout = item.layout()
-                if sublayout is not None:
-                    sublayouts.append(sublayout)
-        return sublayouts
-        
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
 
     window = Window()
     window.show()
-    sys.exit(app.exec_())  
+    sys.exit(app.exec_())
