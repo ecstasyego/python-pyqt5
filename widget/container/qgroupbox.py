31ffd5a984aa0aa0a69ecfcd5b7ccec7bb37fb6d
ecstasyego <70621679+ecstasyego@users.noreply.github.com>
2024-08-15 19:48:31 +0900

    Create qgroupbox.py

---
+++ widget/container/qgroupbox.py
@@ -0,0 +1,20 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Window(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+        layout = QtWidgets.QVBoxLayout()
+        groupBox = QtWidgets.QGroupBox()
+        groupBox.setLayout(QtWidgets.QVBoxLayout())
+        layout.addWidget(groupBox)
+
+        self.setLayout(layout)
+        self.setGeometry(300, 300, 300, 200)
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+
+    window = Window()
+    window.show()
+    sys.exit(app.exec_())
