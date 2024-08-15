d61bec12e87da217e503248ba89db887c49d057a
ecstasyego <70621679+ecstasyego@users.noreply.github.com>
2024-08-15 20:51:35 +0900

    Update qtabwidget.py

--- widget/container/qtabwidget.py
+++ widget/container/qtabwidget.py
@@ -3,11 +3,11 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Window(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
-        layout = QtWidgets.QVBoxLayout()
         widget = QtWidgets.QTabWidget()
         widget.addTab(QtWidgets.QWidget(), 'TAB1')
         widget.addTab(QtWidgets.QWidget(), 'TAB2')
         
+        layout = QtWidgets.QVBoxLayout()
         layout.addWidget(widget)
         self.setLayout(layout)
         self.setGeometry(300, 300, 300, 200)
