572b740304e7b76559cfc1e385be73e03adb60e2
ecstasyego <70621679+ecstasyego@users.noreply.github.com>
2024-08-15 22:48:03 +0900

    Create get_layout.py

---
+++ widget/layout/get_layout.py
@@ -0,0 +1,19 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Window(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setLayout(QtWidgets.QVBoxLayout())
+        self.setGeometry(300, 300, 300, 200)
+
+    def get_layout(self):
+        layout = self.layout()
+        return layout
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+
+    window = Window()
+    window.show()
+    sys.exit(app.exec_())
