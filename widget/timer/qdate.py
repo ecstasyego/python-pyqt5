d4adb5bcb8b8840581c5d2f7cdb5d6a3169bd2f1
ecstasyego <70621679+ecstasyego@users.noreply.github.com>
2024-08-16 01:59:33 +0900

    Update qdate.py

--- widget/timer/qdate.py
+++ widget/timer/qdate.py
@@ -1,1 +1,27 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Window(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+        # DATE TIME
+        date = QtCore.QDate.currentDate().toString('yyyy-MM-dd')
+
+        # WIDGETS
+        widget = QtWidgets.QLabel(self)
+        widget.setText(f'Current Date: {date}')
+
+        # LAYOUT
+        layout = QtWidgets.QVBoxLayout()
+        layout.addWidget(widget)
+        layout.addStretch(1)
+        self.setLayout(layout)
+        self.setGeometry(300, 300, 300, 200)
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+
+    window = Window()
+    window.show()
+    sys.exit(app.exec_())
 
