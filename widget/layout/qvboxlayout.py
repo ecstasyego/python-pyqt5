45b4f6a027ea2c9caabb3867cb58aa945dc68f19
ecstasyego <70621679+ecstasyego@users.noreply.github.com>
2024-08-15 20:23:09 +0900

    Create qvboxlayout.py

---
+++ widget/layout/qvboxlayout.py
@@ -0,0 +1,21 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Window(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+        layout = QtWidgets.QVBoxLayout()
+        layout.addStretch(1)
+        layout.addWidget(QtWidgets.QWidget())
+        layout.addWidget(QtWidgets.QWidget())
+        layout.addStretch(1)
+
+        self.setLayout(layout)
+        self.setGeometry(300, 300, 300, 200)
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+
+    window = Window()
+    window.show()
+    sys.exit(app.exec_())
