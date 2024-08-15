b1370b21edab534b682fd316a1abbb047d0cae2c
ecstasyego <70621679+ecstasyego@users.noreply.github.com>
2024-08-16 00:52:45 +0900

    Update qcombobox.py

--- widget/input/qcombobox.py
+++ widget/input/qcombobox.py
@@ -4,15 +4,20 @@ class Window(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
         # WIDGETS
-        widget = QtWidgets.QComboBox(self)
-        widget.addItem('Option1')
-        widget.addItem('Option2')
-        widget.addItem('Option3')
-        widget.addItem('Option4')
+        self.widgets = dict()
+        self.widgets['widget1'] = QtWidgets.QComboBox(self)
+        self.widgets['widget2'] = QtWidgets.QStatusBar(self)
+
+        self.widgets['widget1'].addItem('Option1')
+        self.widgets['widget1'].addItem('Option2')
+        self.widgets['widget1'].addItem('Option3')
+        self.widgets['widget1'].addItem('Option4')
 
         # LAYOUT
         layout = QtWidgets.QVBoxLayout()
-        layout.addWidget(widget)
+        layout.addWidget(self.widgets['widget1'])
+        layout.addStretch(1)
+        layout.addWidget(self.widgets['widget2'])
         self.setLayout(layout)
         self.setGeometry(300, 300, 300, 200)
 
@@ -20,27 +25,6 @@ class Window(QtWidgets.QWidget):
         status_widget = self.get_widgets()[-1]
         status_widget.showMessage("CallBack", 1000)
 
-    def get_widgets(self, layout=None):
-        layout = self.layout() if layout is None else layout
-        widgets = list()
-        for i in range(layout.count()):
-            item = layout.itemAt(i)
-            if item is not None:
-                widget = item.widget()
-                if widget is not None:
-                    widgets.append(widget)
-        return widgets
-
-    def get_sublayouts(self, layout=None):
-        layout = self.layout() if layout is None else layout
-        sublayouts = list()
-        for i in range(layout.count()):
-            item = layout.itemAt(i)
-            if item is not None:
-                sublayout = item.layout()
-                if sublayout is not None:
-                    sublayouts.append(sublayout)
-        return sublayouts
 
 if __name__ == "__main__":
     import sys
@@ -48,4 +32,4 @@ if __name__ == "__main__":
 
     window = Window()
     window.show()
-    sys.exit(app.exec_())  
+    sys.exit(app.exec_())
