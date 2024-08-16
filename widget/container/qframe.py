8d608cd0cdde89696c196083af1db73010d11cbe
ecstasyego <70621679+ecstasyego@users.noreply.github.com>
2024-08-16 11:58:20 +0900

    Create qframe.py

---
+++ widget/container/qframe.py
@@ -0,0 +1,24 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Window(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+        # WIDGETS
+        widget = QtWidgets.QSplitter(QtCore.Qt.Horizontal)
+        widget.addWidget(QtWidgets.QFrame())
+        widget.addWidget(QtWidgets.QFrame())
+        widget.addWidget(QtWidgets.QFrame())
+
+        # LAYOUTS
+        layout = QtWidgets.QVBoxLayout()
+        layout.addWidget(widget)
+        self.setLayout(layout)
+        self.setGeometry(300, 300, 300, 200)
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+
+    window = Window()
+    window.show()
+    sys.exit(app.exec_())                                                                                                                                              
