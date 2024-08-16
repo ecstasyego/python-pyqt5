9a0a4abb475c108bbfbeae527fc84e44d9e8c0f1
ecstasyego <70621679+ecstasyego@users.noreply.github.com>
2024-08-16 11:43:26 +0900

    Update qlabel.py

--- widget/display/qlabel.py
+++ widget/display/qlabel.py
@@ -7,6 +7,7 @@ class Window(QtWidgets.QWidget):
         self.widgets = dict()
         self.widgets['widget1'] = QtWidgets.QLabel('First Label', self)
         self.widgets['widget2'] = QtWidgets.QLabel('Second Label', self)
+        self.widgets['widget3'] = QtWidgets.QLabel(self)
 
         # WIDGETS: Properties
         font1 = self.widgets['widget1'].font(); font1.setPointSize(20)
@@ -15,12 +16,14 @@ class Window(QtWidgets.QWidget):
         self.widgets['widget2'].setFont(font2)
         self.widgets['widget1'].setAlignment(QtCore.Qt.AlignCenter)
         self.widgets['widget2'].setAlignment(QtCore.Qt.AlignVCenter)
+        self.widgets['widget3'].setText('Third Label')
 
 
         # LAYOUTS
         layout = QtWidgets.QVBoxLayout()
         layout.addWidget(self.widgets['widget1'])
         layout.addWidget(self.widgets['widget2'])
+        layout.addWidget(self.widgets['widget3'])
         layout.addStretch(1)
         self.setLayout(layout)
         self.setGeometry(300, 300, 300, 200)
