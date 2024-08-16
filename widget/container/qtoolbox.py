e537c1c796215825be61e106fd2327a1e0eda48d
ecstasyego <70621679+ecstasyego@users.noreply.github.com>
2024-08-16 22:45:54 +0900

    Update qtoolbox.py

--- widget/container/qtoolbox.py
+++ widget/container/qtoolbox.py
@@ -7,6 +7,22 @@ class Window(QtWidgets.QWidget):
         widget = QtWidgets.QToolBox()
         widget.addItem(QtWidgets.QWidget(), "ITEM1")
         widget.addItem(QtWidgets.QWidget(), "ITEM2")
+        widget.addItem(QtWidgets.QWidget(), "ITEM3")
+        widget.setStyleSheet("""
+            QToolBox {
+                border: 2px solid #333;
+                background-color: #f0f0f0;
+            }
+            QToolBox::tab {
+                background-color: #c0c0c0;
+                padding: 5px;
+                border: 1px solid #aaa;
+            }
+            QToolBox::tab:selected {
+                background-color: #e0e0e0;
+                border: 1px solid #555;
+            }
+        """)
 
         # LAYOUTS
         layout = QtWidgets.QVBoxLayout()
