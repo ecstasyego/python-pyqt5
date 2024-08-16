58223d0387432e5abed327d0e3df30a816b111a8
ecstasyego <70621679+ecstasyego@users.noreply.github.com>
2024-08-16 11:10:47 +0900

    Update qtimeedit.py

--- widget/input/qtimeedit.py
+++ widget/input/qtimeedit.py
@@ -14,7 +14,7 @@ class Window(QtWidgets.QWidget):
         self.widgets['widget1'].setDisplayFormat('hh:mm:ss')
         self.widgets['widget1'].timeChanged.connect(self.callback)
 
-        # LAYOUT
+        # LAYOUTS
         layout = QtWidgets.QVBoxLayout()
         layout.addWidget(self.widgets['widget1'])
         layout.addStretch(1)
