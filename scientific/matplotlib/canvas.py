709e2566dd4997ad7542acd27bdde8bce05a4516
ecstasyego <70621679+ecstasyego@users.noreply.github.com>
2024-08-16 11:00:56 +0900

    Update canvas.py

--- scientific/matplotlib/canvas.py
+++ scientific/matplotlib/canvas.py
@@ -1,56 +1,60 @@
-import sys
-from PyQt5.QtWidgets import *
-from PyQt5.QtGui import *
-import numpy as np
-import matplotlib.pyplot as plt
+from PyQt5 import QtCore, QtGui, QtWidgets
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+import matplotlib.pyplot as plt
+import numpy as np
 
-class Window(QWidget):
+class Window(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
-        self.initUI()
+        self.fig = plt.Figure()
 
-    def initUI(self):
+        # WIDGETS: Declaration
+        self.widgets = dict()
+        self.widgets['widget1'] = FigureCanvas(self.fig)
+        self.widgets['widget2'] = QtWidgets.QLineEdit(self)
+        self.widgets['widget3'] = QtWidgets.QPushButton('Draw', self)
+
+        # WIDGETS: Properties
+        self.widgets['widget3'].clicked.connect(self.callback)
+
+
+        # LAYOUT: Declaration
+        self.layouts = dict()
+        self.layouts['layout0'] = QtWidgets.QHBoxLayout()
+        self.layouts['layout1'] = QtWidgets.QVBoxLayout()
+        self.layouts['layout2'] = QtWidgets.QVBoxLayout()
+
+        # LAYOUT: Construction
+        self.layouts['layout0'].addLayout(self.layouts['layout1'])
+        self.layouts['layout0'].addLayout(self.layouts['layout2'])
+        self.layouts['layout0'].setStretchFactor(self.layouts['layout1'], 1)
+        self.layouts['layout0'].setStretchFactor(self.layouts['layout2'], 0)
+
+        self.layouts['layout1'].addWidget(self.widgets['widget1'])
+        self.layouts['layout2'].addWidget(self.widgets['widget2'])
+        self.layouts['layout2'].addWidget(self.widgets['widget3'])
+        self.layouts['layout2'].addStretch(1)
+
+        layout = self.layouts['layout0']
+        self.setLayout(layout)
         self.setGeometry(600, 200, 1200, 600)
         self.setWindowTitle("PyChart Viewer v0.1")
-        self.setWindowIcon(QIcon('icon.png'))
 
-        self.lineEdit = QLineEdit()
-        self.pushButton = QPushButton("차트그리기")
-        self.pushButton.clicked.connect(self.pushButtonClicked)
-
-        self.fig = plt.Figure()
-        self.canvas = FigureCanvas(self.fig)
-
-        leftLayout = QVBoxLayout()
-        leftLayout.addWidget(self.canvas)
-
-        # Right Layout
-        rightLayout = QVBoxLayout()
-        rightLayout.addWidget(self.lineEdit)
-        rightLayout.addWidget(self.pushButton)
-        rightLayout.addStretch(1)
-
-        layout = QHBoxLayout()
-        layout.addLayout(leftLayout)
-        layout.addLayout(rightLayout)
-        layout.setStretchFactor(leftLayout, 1)
-        layout.setStretchFactor(rightLayout, 0)
-
-        self.setLayout(layout)
-
-    def pushButtonClicked(self):
+    def callback(self):
+        self.fig.clf()
         ax = self.fig.add_subplot(111)
         ax.plot(np.random.normal(size=100).cumsum())
         ax.legend(loc='upper right')
         ax.grid()
 
-        self.canvas.draw()
+        canvas = self.widgets['widget1']
+        canvas.draw()
 
 
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+
     window = Window()
     window.show()
-    app.exec_()
-
+    sys.exit(app.exec_())
