b863478aee8e81d4a5bdbc51e80ceb87f77d2531
ecstasyego <70621679+ecstasyego@users.noreply.github.com>
2024-08-17 11:18:45 +0900

    Update qtablewidget.py

--- widget/display/qtablewidget.py
+++ widget/display/qtablewidget.py
@@ -4,18 +4,17 @@ class Window(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
         # WIDGETS
-        self.widgets = dict()
-        self.widgets['widget1'] = QtWidgets.QTableWidget()
-        self.widgets['widget1'].setRowCount(20)
-        self.widgets['widget1'].setColumnCount(4)
+        widget = QtWidgets.QTableWidget()
+        widget.setRowCount(20)
+        widget.setColumnCount(4)
         for i in range(20):
             for j in range(4):
-                self.widgets['widget1'].setItem(i, j, QtWidgets.QTableWidgetItem(str(i+j)))
-        self.widgets['widget1'].horizontalHeader().setSectionResizeMode(QtWidgets.QHeaderView.Stretch)
+                widget.setItem(i, j, QtWidgets.QTableWidgetItem(str(i+j)))
+        widget.horizontalHeader().setSectionResizeMode(QtWidgets.QHeaderView.Stretch)
 
         # LAYOUTS
         layout = QtWidgets.QVBoxLayout()
-        layout.addWidget(self.widgets['widget1'])
+        layout.addWidget(widget)
         layout.addStretch(1)
 
         self.setLayout(layout)
@@ -31,3 +30,4 @@ if __name__ == "__main__":
     window = Window()
     window.show()
     sys.exit(app.exec_())
+                                                                                                                                                     66,25         Bot 
