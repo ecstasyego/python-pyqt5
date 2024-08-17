71903a7f2dce0ddab507eae3e90cfcd5dccf0649
ecstasyego <70621679+ecstasyego@users.noreply.github.com>
2024-08-17 10:58:47 +0900

    Create qlistview.py

---
+++ widget/display/qlistview.py
@@ -0,0 +1,30 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Window(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+        # ITEM MODEL
+        model = QtGui.QStandardItemModel()
+        model.appendRow(QtGui.QStandardItem('Item 1.1'))
+        model.appendRow(QtGui.QStandardItem('Item 1.2'))
+        model.appendRow(QtGui.QStandardItem('Item 1.3'))
+        model.appendRow(QtGui.QStandardItem('Item 1.4'))
+        model.appendRow(QtGui.QStandardItem('Item 1.5'))
+
+        # WIDGETS
+        widget = QtWidgets.QListView()
+        widget.setModel(model)
+
+        # LAYOUTS
+        layout = QtWidgets.QVBoxLayout()
+        layout.addWidget(widget)
+        self.setLayout(layout)
+        self.setGeometry(300, 300, 300, 200)
+
+    def callback(self):                                                                                                                                                        pass
+                                                                                                                                                                       if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+                                                                                                                                                                           window = Window()
+    window.show()
+    sys.exit(app.exec_())                                                                                                                                              
