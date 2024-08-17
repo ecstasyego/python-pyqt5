18b9bdb5e10a0e9bf6038c94f2ff311cc1fa3af6
ecstasyego <70621679+ecstasyego@users.noreply.github.com>
2024-08-17 19:39:14 +0900

    Update otreewrapper.py

--- widget/otreewrapper.py
+++ widget/otreewrapper.py
@@ -1,6 +1,23 @@
 class OTreeWrapper:
     def __init__(self, node=None):
         self.node = node
+        self.generation = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        return list(self.__sibling.values())[self.order+1]
+
+    def __repr__(self):
+        representation = f"""
+        - NAME: {self.name}
+        - ACCESS SPECIFIER: {'.'.join(self.ascendants)}.{self.name}
+        - GENERATION: {self.generation} + 1
+        - ASCENDANTS: {'.'.join(self.ascendants)}
+        - PARENT: {self.parent.name}
+        """
+        return representation
 
     @property
     def name(self):
@@ -18,18 +35,45 @@ class OTreeWrapper:
     def parent(self, other):
         self.__parent = other
 
+    @property
+    def sibling(self):
+        return self.__sibling
+
+    @sibling.setter
+    def sibling(self, other):
+        self.__sibling = other
+
     @classmethod
     def progeny(cls, self):
-        members = list(filter(lambda x: not x.startswith('_OTreeWrapper__') and x != 'parent' and x != 'name', dir(self)))
+        children = cls.posterity(self)
+        while children:
+            _children = list()
+            for child in children:
+                _children.extend(cls.posterity(child))
+            children = _children
+
+    @classmethod
+    def posterity(cls, self):
+        if self.generation == 0:
+            self.name = 'ROOT'
+
+        members = list(filter(lambda x: not x.startswith('_OTreeWrapper__') and x != 'parent' and x != 'sibling' and x != 'name', dir(self)))
         self.children = dict(map(lambda x: (x, getattr(self, x)), filter(lambda x: isinstance(getattr(self, x), cls), members)))
-        if len(self.children):
-            for name, child in self.children.items():
-                child.parent = self
-                child.name = name
-                return cls.progeny(child)
-        else:
-            return
-            
+        for order, (name, child) in enumerate(self.children.items()):
+            child.parent = self
+            child.generation = self.generation + 1
+            child.ascendants = self.ascendants + [ self.name ] if self.generation else [ self.name ]
+            child.name = name
+            child.order = order
+            child.sibling = self.children
+
+            self.connection(child) # OTree Node Connection
+        return self.children.values()
+
+    def connection(self, child):
+        self.node
+        child.node
+
 root = OTreeWrapper()
 root.a = OTreeWrapper()
 root.a.a = OTreeWrapper()
@@ -39,7 +83,10 @@ root.a.b.b = OTreeWrapper()
 root.a.b.b.a = OTreeWrapper()
 root.a.c = OTreeWrapper()
 root.a.d = OTreeWrapper()
+root.b = OTreeWrapper()
+root.b.a = OTreeWrapper()
+root.b.b = OTreeWrapper()
+root.b.c = OTreeWrapper()
 OTreeWrapper.progeny(root)
 
-print(root.a.parent)
-print(root.a.children)
+print(root.a.b.b.a)
