17fa7b28d824f5cdca3a034e75a0b2de5f252ee4
ecstasyego <70621679+ecstasyego@users.noreply.github.com>
2024-08-25 20:16:09 +0900

    Update resizeevent.py

--- widget/event_handler/resizeevent.py
+++ widget/event_handler/resizeevent.py
@@ -1,1 +1,21 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
 
+class Window(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setLayout(QtWidgets.QVBoxLayout()) # LAYOUT
+        self.setGeometry(300, 300, 300, 200)
+
+    def resizeEvent(self, event):
+        super().resizeEvent(event)
+        new_size = event.size()
+        print(f"Widget resized to {new_size.width()}x{new_size.height()}")
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+
+    window = Window()
+    window.show()
+    sys.exit(app.exec_())
