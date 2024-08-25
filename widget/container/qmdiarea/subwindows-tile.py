db89bd1963d5d452fdf109588a141bbd36fec634
ecstasyego <70621679+ecstasyego@users.noreply.github.com>
2024-08-26 00:02:57 +0900

    Create subwindows-tile.py

---
+++ widget/container/qmdiarea/subwindows-tile.py
@@ -0,0 +1,32 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Window(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+        widget = QtWidgets.QMdiArea(self)
+        button = QtWidgets.QPushButton("PRESS")
+        button.clicked.connect(self.callback)
+        self.widget = widget
+
+        layout = QtWidgets.QVBoxLayout()
+        layout.addWidget(widget)
+        layout.addWidget(button)
+        self.setLayout(layout)
+
+    def callback(self):
+        sub_window = QtWidgets.QMdiSubWindow()
+        sub_window.setWidget(QtWidgets.QLabel('LABEL'))
+        sub_window.setWindowTitle(f"MDI")
+        self.widget.addSubWindow(sub_window)
+
+        sub_window.show()
+        self.widget.tileSubWindows()
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+
+    window = Window()
+    window.show()
+    sys.exit(app.exec_())
