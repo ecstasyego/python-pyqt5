978254fe5d751b3583860f573ad3a7b847b7b82b
ecstasyego <70621679+ecstasyego@users.noreply.github.com>
2024-08-26 00:34:51 +0900

    Update eventfilter.py

--- widget/event_handler/eventfilter.py
+++ widget/event_handler/eventfilter.py
@@ -1,1 +1,22 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
 
+class CustomEventFilter(QtCore.QObject):
+    def eventFilter(self, obj, event):
+        if event.type() == QtCore.QEvent.MouseButtonPress:
+            print(f"Mouse button pressed on: {obj.objectName()}")
+        return super().eventFilter(obj, event)
+
+class Window(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+        self.installEventFilter(CustomEventFilter(self))
+        self.setLayout(QtWidgets.QVBoxLayout()) # LAYOUT
+        self.setGeometry(300, 300, 300, 200)
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+
+    window = Window()
+    window.show()
+    sys.exit(app.exec_())
