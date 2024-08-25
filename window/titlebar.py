933797e0defdfde69276522a3f8e5b2b2b8d4c96
ecstasyego <70621679+ecstasyego@users.noreply.github.com>
2024-08-25 23:12:00 +0900

    Create titlebar.py

---
+++ window/titlebar.py
@@ -0,0 +1,15 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Window(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle('Window Title')
+        self.setGeometry(300, 300, 300, 200)
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+
+    window = Window()
+    window.show()
+    sys.exit(app.exec_())
