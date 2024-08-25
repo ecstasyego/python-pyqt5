b776045a420dc781f0848c125664b1a165aa8b60
ecstasyego <70621679+ecstasyego@users.noreply.github.com>
2024-08-25 20:26:30 +0900

    Update moveevent.py

--- widget/event_handler/moveevent.py
+++ widget/event_handler/moveevent.py
@@ -1,1 +1,19 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
 
+class Window(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setLayout(QtWidgets.QVBoxLayout()) # LAYOUT
+        self.setGeometry(300, 300, 300, 200)
+
+    def moveEvent(self, event):
+        new_pos = self.pos()
+        print(f"Widget moved to: ({new_pos.x()}, {new_pos.y()})")
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+
+    window = Window()
+    window.show()
+    sys.exit(app.exec_())
