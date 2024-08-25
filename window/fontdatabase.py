8184f49ca6ff405be10d0e9ac402950d4ce72b14
ecstasyego <70621679+ecstasyego@users.noreply.github.com>
2024-08-25 22:52:32 +0900

    Create fontdatabase.py

---
+++ window/fontdatabase.py
@@ -0,0 +1,32 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Widget(QtWidgets.QWidget):
+    def __init__(self, other):
+        super(Widget, self).__init__(other)
+        widget = QtWidgets.QLabel('Hello, World!')
+        layout = QtWidgets.QVBoxLayout()
+        layout.addWidget(widget)
+        self.setLayout(layout)
+
+class Window(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+        centralwidget = Widget(self)
+        self.setCentralWidget(centralwidget)
+        self.setMenuBar(QtWidgets.QMenuBar(self))
+        self.setStatusBar(QtWidgets.QStatusBar(self))
+        QtCore.QMetaObject.connectSlotsByName(self)
+
+
+if __name__ == "__main__":
+
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+
+    QFontDB = QtGui.QFontDatabase()
+    QFontDB.addApplicationFont('NanumGothic.ttf')
+    app.setFont(QtGui.QFont("NanumGothic", 12))
+
+    window = Window()
+    window.show()
+    sys.exit(app.exec_())
