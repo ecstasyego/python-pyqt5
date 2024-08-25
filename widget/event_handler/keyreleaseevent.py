90ca8065c599d853cc2fcc915b6f852114b2283f
ecstasyego <70621679+ecstasyego@users.noreply.github.com>
2024-08-25 20:18:56 +0900

    Update keyreleaseevent.py

--- widget/event_handler/keyreleaseevent.py
+++ widget/event_handler/keyreleaseevent.py
@@ -1,1 +1,21 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
 
+class Window(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setLayout(QtWidgets.QVBoxLayout()) # LAYOUT
+        self.setGeometry(300, 300, 300, 200)
+
+    def keyReleaseEvent(self, event: QtGui.QKeyEvent):
+        key = event.key()
+        text = event.text()
+        print(f"Key released: {key} ({text})")
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+
+    window = Window()
+    window.show()
+    sys.exit(app.exec_())
