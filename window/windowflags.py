909cc65875cf15cbe3255336479e6708df3b38e8
ecstasyego <70621679+ecstasyego@users.noreply.github.com>
2024-08-26 01:04:03 +0900

    Update windowflags.py

--- window/windowflags.py
+++ window/windowflags.py
@@ -1,1 +1,18 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
 
+class Window(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowFlags(self.windowFlags() & ~QtCore.Qt.WindowMinimizeButtonHint & ~QtCore.Qt.WindowMaximizeButtonHint & ~QtCore.Qt.WindowCloseButtonHint )
+
+        self.setWindowTitle('Window Title')
+        self.setGeometry(300, 300, 300, 200)
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+
+    window = Window()
+    window.show()
+    sys.exit(app.exec_())
