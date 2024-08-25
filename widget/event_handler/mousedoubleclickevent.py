d71d366ce6074d1f1b5dcfa83d2089fe54237840
ecstasyego <70621679+ecstasyego@users.noreply.github.com>
2024-08-25 20:20:00 +0900

    Update mousedoubleclickevent.py

--- widget/event_handler/mousedoubleclickevent.py
+++ widget/event_handler/mousedoubleclickevent.py
@@ -1,1 +1,20 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
 
+class Window(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setLayout(QtWidgets.QVBoxLayout()) # LAYOUT
+        self.setGeometry(300, 300, 300, 200)
+
+    def mouseDoubleClickEvent(self, event: QtGui.QMouseEvent):
+        pos = event.pos()
+        print(f"Mouse double clicked at: ({pos.x()}, {pos.y()})")
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+
+    window = Window()
+    window.show()
+    sys.exit(app.exec_())
