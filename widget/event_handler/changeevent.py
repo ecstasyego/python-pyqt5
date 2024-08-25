594b9aa7c218575c5f05c1fa96b829543a491b75
ecstasyego <70621679+ecstasyego@users.noreply.github.com>
2024-08-25 21:09:34 +0900

    Update changeevent.py

--- widget/event_handler/changeevent.py
+++ widget/event_handler/changeevent.py
@@ -1,3 +1,24 @@
+"""
+[PyQt5.QtCore.Qt]
+- Window States: Qt.WindowNoState, Qt.WindowMinimizeda, Qt.WindowMaximized, Qt.WindowFullScreen
+- Window Flags: Qt.Window, Qt.Dialog, Qt.Popup, Qt.Tool, Qt.Sheet, Qt.FramelessWindowHint, Qt.WindowStaysOnTopHint
+- Widget States(1): Qt.Active, Qt.Inactive
+- Widget States(2): Qt.Widget, Qt.Tool, Qt.Dialog, Qt.Popup, Qt.Sheet
+- Alignment: Qt.AlignLeft, Qt.AlignRight, Qt.AlignTop, Qt.AlignBottom, Qt.AlignCenter, Qt.AlignHCenter, Qt.AlignVCenter
+- Button States: Qt.Checked, Qt.Unchecked, Qt.PartiallyChecked
+- Modes: Qt.NormalMode, Qt.CompactMode
+_ Register Modes: Qt.LeftToRight, Qt.RightToLeft
+- Key Codes: Qt.Key_Enter, Qt.Key_Escape, Qt.Key_Space, Qt.Key_Tab
+
+[PyQt5.QtCore.QEvent]
+- QEvent.None, QEvent.FocusIn, QEvent.FocusOut,
+- QEvent.MouseButtonPress, QEvent.MouseButtonRelease, QEvent.MouseButtonDblClick, QEvent.MouseMove,
+- QEvent.KeyPress, QEvent.KeyRelease,
+- QEvent.Enter, QEvent.Close, QEvent.Leave, QEvent.Resize, QEvent.Move, QEvent.Paint, QEvent.Wheel,
+- QEvent.WindowStateChange,
+- QEvent.Show, QEvent.Hide, QEvent.ContextMenu
+"""
+
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 class Window(QtWidgets.QWidget):
