5b2e1e24ebc727c02328dcfacf4211bbeeb3bc7b
ecstasyego <70621679+ecstasyego@users.noreply.github.com>
2024-08-27 21:26:15 +0900

    Update contentsmargins.py

--- widget/contentsmargins.py
+++ widget/contentsmargins.py
@@ -1,1 +1,20 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
 
+class Window(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+        widget = QtWidgets.QTextEdit()
+        layout = QtWidgets.QVBoxLayout()
+        layout.addWidget(widget)
+        layout.setContentsMargins(0, 0, 0, 0)
+
+        self.setLayout(layout) # LAYOUT
+        self.setGeometry(300, 300, 300, 200)
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+
+    window = Window()
+    window.show()
+    sys.exit(app.exec_())
