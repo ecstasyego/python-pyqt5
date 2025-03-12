29e12bb7bdd53b132e4592ff837b341b4d52003a
ecstasyego <70621679+ecstasyego@users.noreply.github.com>
2025-03-12 15:55:05 +0900

    Update qtableview.py

--- server/qtableview.py
+++ server/qtableview.py
@@ -1,1 +1,35 @@
+from PyQt5 import QtCore, QtGui, QtWidgets, QtSql
 
+class Window(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+        # Database
+        db = QtSql.QSqlDatabase.addDatabase("QSQLITE")
+        db.setDatabaseName("example.db")
+        db.open()
+
+        # Model
+        model = QtSql.QSqlTableModel()
+        model.setTable("users")
+        model.select()
+
+        # WIDGETS
+        widget = QtWidgets.QTableView()
+        widget.setModel(model)
+
+        # LAYOUTS
+        layout = QtWidgets.QVBoxLayout()
+        layout.addWidget(widget)
+        self.setLayout(layout)
+        self.setGeometry(300, 300, 300, 200)
+
+    def callback(self):
+        pass
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+
+    window = Window()
+    window.show()
+    sys.exit(app.exec_())
