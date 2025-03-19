bbece7f7a97a7cd27cf046e436d7b9ac0a5b4719
ecstasyego <70621679+ecstasyego@users.noreply.github.com>
2025-03-19 15:25:14 +0900

    Update sender_and_receiver.py

--- server/pyqtsignal/sender_and_receiver.py
+++ server/pyqtsignal/sender_and_receiver.py
@@ -1,47 +1,24 @@
-from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5 import QtCore
 
 class Sender(QtCore.QObject):
+    # [1] signal.connect: signal.connect(lambda *values: print(values))
+    # [2] signal.emit
     signal = QtCore.pyqtSignal(str)
 
     def __init__(self):
         super().__init__()
 
-    def send_signal(self, message):
-        self.signal.emit(message)
+    def send_signal(self):
+        self.signal.emit("Hello, World!")
 
 class Receiver(QtCore.QObject):
     def __init__(self):
         super().__init__()
 
-    def receive_signal(self, message):
-        print(f"Signal received with message: {message}")
+    def receive_signal(self, *values):
+        print(values)
 
-class Window(QtWidgets.QWidget):
-    def __init__(self):
-        super().__init__()
-        # SIGNAL
-        self.sender = Sender()
-        self.receiver = Receiver()
-        self.sender.signal.connect(self.receiver.receive_signal)
-
-        # WIDGET
-        self.button = QtWidgets.QPushButton("Send Signal")
-        self.button.clicked.connect(self.callback)
-
-        # LAYOUT
-        self.layout = QtWidgets.QVBoxLayout()
-        self.layout.addWidget(self.button)
-
-        self.setLayout(self.layout)
-        self.setGeometry(300, 300, 300, 200)
-
-    def callback(self):
-        self.sender.send_signal("Hello from Sender!")
-
-if __name__ == "__main__":
-    import sys
-    app = QtWidgets.QApplication(sys.argv)
-
-    window = Window()
-    window.show()
-    sys.exit(app.exec_())
+receiver = Receiver()
+sender = Sender()
+sender.signal.connect(receiver.receive_signal)
+sender.send_signal()
