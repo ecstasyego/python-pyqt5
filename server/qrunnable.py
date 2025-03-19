18f39ba6cdf0b0038938b2de409eae914a64f3b2
ecstasyego <70621679+ecstasyego@users.noreply.github.com>
2025-03-19 16:16:51 +0900

    Update qrunnable.py

--- server/qrunnable.py
+++ server/qrunnable.py
@@ -1,1 +1,26 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
 
+class Worker(QtCore.QRunnable):
+    def __init__(self):
+        super().__init__()
+
+    def run(self):
+        QtCore.QThread.sleep(3)
+
+class Window(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.worker = Worker()
+        self.pool = QtCore.QThreadPool.globalInstance()
+        self.pool.start(self.worker)
+        self.pool.waitForDone()  # JOIN
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+
+    window = Window()
+    window.show()
+    sys.exit(app.exec_())
