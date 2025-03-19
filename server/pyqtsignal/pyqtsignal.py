ea2002a086a651f8d9822d9634a799677ef62349
ecstasyego <70621679+ecstasyego@users.noreply.github.com>
2025-03-19 15:15:59 +0900

    Update pyqtsignal.py

--- server/pyqtsignal/pyqtsignal.py
+++ server/pyqtsignal/pyqtsignal.py
@@ -1,6 +1,6 @@
 from PyQt5 import QtCore
 
-class Communicate(QtCore.QObject):
+class Sender(QtCore.QObject):
     signal_int = QtCore.pyqtSignal(int)
     signal_str = QtCore.pyqtSignal(str)
     signal_composite = QtCore.pyqtSignal(int, str, float)
@@ -9,37 +9,37 @@ class Communicate(QtCore.QObject):
     def __init__(self):
         super().__init__()
 
-    def emit_signal(self):
+    def send_signal(self):
         self.signal_int.emit(42)
         self.signal_str.emit("Hello")
         self.signal_composite.emit(100, "World", 3.14)
         self.signals[int].emit(99)
         self.signals[str].emit("Hello, World!")
 
-def receive_slot(*values):
+def receive_signal(*values):
     print(f"SIGNAL: {values}")
 
-com = Communicate()
+sender = Sender()
 print("[Connection] Slot")
-com.signal_int.connect(receive_slot)
-com.signal_str.connect(receive_slot)
-com.signal_composite.connect(receive_slot)
-com.signals[int].connect(receive_slot)
-com.signals[str].connect(receive_slot)
-com.emit_signal()
+sender.signal_int.connect(receive_signal)
+sender.signal_str.connect(receive_signal)
+sender.signal_composite.connect(receive_signal)
+sender.signals[int].connect(receive_signal)
+sender.signals[str].connect(receive_signal)
+sender.send_signal()
 
 print("[Disconnection]")
-com.signal_int.disconnect(receive_slot)
-com.signal_str.disconnect(receive_slot)
-com.signal_composite.disconnect(receive_slot)
-com.signals[int].disconnect(receive_slot)
-com.signals[str].disconnect(receive_slot)
-com.emit_signal()
+sender.signal_int.disconnect(receive_signal)
+sender.signal_str.disconnect(receive_signal)
+sender.signal_composite.disconnect(receive_signal)
+sender.signals[int].disconnect(receive_signal)
+sender.signals[str].disconnect(receive_signal)
+sender.send_signal()
 
 print("[Connection] Lambda")
-com.signal_int.connect(lambda *values: print(f"SIGNAL: {values}"))
-com.signal_str.connect(lambda *values: print(f"SIGNAL: {values}"))
-com.signal_composite.connect(lambda *values: print(f"SIGNAL: {values}"))
-com.signals[int].connect(lambda *values: print(f"SIGNAL: {values}"))
-com.signals[str].connect(lambda *values: print(f"SIGNAL: {values}"))
-com.emit_signal()
+sender.signal_int.connect(lambda *values: print(f"SIGNAL: {values}"))
+sender.signal_str.connect(lambda *values: print(f"SIGNAL: {values}"))
+sender.signal_composite.connect(lambda *values: print(f"SIGNAL: {values}"))
+sender.signals[int].connect(lambda *values: print(f"SIGNAL: {values}"))
+sender.signals[str].connect(lambda *values: print(f"SIGNAL: {values}"))
+sender.send_signal()
