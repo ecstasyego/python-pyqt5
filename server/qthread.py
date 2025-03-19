cc7ae85c510892021bee280d34e9fe598c4bd25f
ecstasyego <70621679+ecstasyego@users.noreply.github.com>
2025-03-19 16:14:54 +0900

    Update qthread.py

--- server/qthread.py
+++ server/qthread.py
@@ -1,1 +1,23 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
 
+class Worker(QtCore.QThread):
+    def __init__(self):
+        super().__init__()
+
+    def run(self):
+        self.msleep(3000)
+
+class Window(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+        self.worker = Worker()
+        self.worker.start()
+        self.worker.wait() # JOIN
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+
+    window = Window()
+    window.show()
+    sys.exit(app.exec_())
