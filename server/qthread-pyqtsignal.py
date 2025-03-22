73b77ee976e1273a6a6f3c4546995f8e1c633785
ecstasyego <70621679+ecstasyego@users.noreply.github.com>
2025-03-22 15:50:40 +0900

    Update qthread-pyqtsignal.py

--- server/qthread-pyqtsignal.py
+++ server/qthread-pyqtsignal.py
@@ -1,1 +1,27 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
 
+class Worker(QtCore.QThread):
+    signal = QtCore.pyqtSignal(list)
+
+    def __init__(self):
+        super().__init__()
+
+    def run(self):
+        self.msleep(3000)
+        self.signal.emit(["Item00", "Item01", "Item02"])
+
+class Window(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+        self.worker = Worker()
+        self.worker.signal.connect(lambda *values: print(values))
+        self.worker.start()
+        self.worker.wait() # JOIN
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+
+    window = Window()
+    window.show()
+    sys.exit(app.exec_())
