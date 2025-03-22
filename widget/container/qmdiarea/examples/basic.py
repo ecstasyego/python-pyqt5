502e1838763dd9c44bebb0981ddd5181186ed217
ecstasyego <70621679+ecstasyego@users.noreply.github.com>
2025-03-22 21:47:22 +0900

    Create basic.py

---
+++ widget/container/qmdiarea/examples/basic.py
@@ -0,0 +1,40 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class SubWindow(QtWidgets.QMdiSubWindow):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        widget = QtWidgets.QWidget()
+        widget.setLayout(QtWidgets.QVBoxLayout())
+        layout = widget.layout()
+        layout.addWidget(QtWidgets.QLabel("LABEL00"))
+        layout.addWidget(QtWidgets.QLabel("LABEL01"))
+        layout.addWidget(QtWidgets.QLabel("LABEL02"))
+
+        self.setWidget(widget)
+        self.setWindowTitle(f"MDI")
+
+class Window(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+        widget = QtWidgets.QMdiArea(self)
+        button = QtWidgets.QPushButton("PRESS")
+        button.clicked.connect(self.callback)
+        self.widget = widget
+
+        layout = QtWidgets.QVBoxLayout()
+        layout.addWidget(widget)
+        layout.addWidget(button)
+        self.setLayout(layout)
+
+    def callback(self):
+        sub_window = SubWindow()
+        self.widget.addSubWindow(sub_window)
+        sub_window.show()
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+
+    window = Window()
+    window.show()
+    sys.exit(app.exec_())
