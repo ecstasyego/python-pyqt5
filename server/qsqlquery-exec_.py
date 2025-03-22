7b817882d077bf90d0c9524b98931226be06c4d2
ecstasyego <70621679+ecstasyego@users.noreply.github.com>
2025-03-22 18:18:45 +0900

    Update qsqlquery-exec_.py

--- server/qsqlquery-exec_.py
+++ server/qsqlquery-exec_.py
@@ -1,1 +1,28 @@
+from PyQt5 import QtSql
 
+db = QtSql.QSqlDatabase.addDatabase( "QSQLITE" )
+db.setDatabaseName(":memory:") # example.db, example.sqlite3, ...
+db.open()
+
+query = QtSql.QSqlQuery()
+query.exec_(
+    """
+    CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT,
+        age INTEGER
+    )
+    """
+)
+query.exec_("INSERT INTO users (name, age) VALUES ('Alice', 30)")
+query.exec_("INSERT INTO users (name, age) VALUES ('Bob', 25)")
+query.exec_("SELECT * FROM users")
+
+while query.next():
+    user_id = query.value(0)  # First column (ID)
+    name = query.value(1)     # Second column (name)
+    age = query.value(2)      # Third column (age)
+    print(f"ID: {user_id}, Name: {name}, Age: {age}")
+
+
+db.close()
