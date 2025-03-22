b5344f6b35081443fef71a3defb7a78bc67c9cf6
ecstasyego <70621679+ecstasyego@users.noreply.github.com>
2025-03-22 18:20:50 +0900

    Update qsqlquery-prepare.py

--- server/qsqlquery-prepare.py
+++ server/qsqlquery-prepare.py
@@ -1,1 +1,34 @@
+from PyQt5 import QtSql
 
+db = QtSql.QSqlDatabase.addDatabase( "QSQLITE" )
+db.setDatabaseName(":memory:") # example.db, example.sqlite3, ...
+db.open()
+
+query = QtSql.QSqlQuery()
+query.exec_(
+    """
+    CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT,
+        age INTEGER
+    )
+    """
+)
+query.prepare("INSERT INTO users (name, age) VALUES (?, ?)")
+
+query.addBindValue("Alice")
+query.addBindValue(25)
+query.exec_()
+
+query.addBindValue("Bob")
+query.addBindValue(30)
+query.exec_()
+
+query.exec_("SELECT * FROM users")
+while query.next():
+    user_id = query.value(0)  # First column (ID)
+    name = query.value(1)     # Second column (name)
+    age = query.value(2)      # Third column (age)
+    print(f"ID: {user_id}, Name: {name}, Age: {age}")
+
+db.close()
